d90d93bce1799a223e1d53af374a3bcf8ce71bda
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-07-23 21:13:12 +0900

    TcpStream를 이용한 Response출력

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -26,12 +25,23 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::OK,
+                                        Some("<h1>IT WORKS</h1>".to_string())
+                                    )
                                 }
-                                Err(e) => println!("Failed to parse a request: {}", e),
-                            }
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            };
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
