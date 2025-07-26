553b276de614ccac83b708b895cbb9a201cca88f
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-07-26 15:19:37 +0900

    Request Handler 구현

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 
 use server::Server;
+use wensite_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod wensite_handler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
